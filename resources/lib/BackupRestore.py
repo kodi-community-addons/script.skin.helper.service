844cbd3e19912be2eca98a1db444df859b60f384
marcelveldt <m.vanderveldt@outlook.com>
2015-10-19 22:51:57 +0200

    attempt to fix unicode issue in os.walk

--- resources/lib/BackupRestore.py
+++ resources/lib/BackupRestore.py
@@ -320,7 +320,7 @@ def restore(silent=None):
 def zip(src, dst):
     zf = zipfile.ZipFile("%s.zip" % (dst), "w", zipfile.ZIP_DEFLATED)
     abs_src = os.path.abspath(src)
-    for dirname, subdirs, files in os.walk(src):
+    for dirname, subdirs, files in os.walk(src.encode(sys.getfilesystemencoding())):
         for filename in files:
             absname = os.path.abspath(os.path.join(dirname, filename))
             arcname = absname[len(abs_src) + 1:]
