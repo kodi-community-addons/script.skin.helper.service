f343c373952cb892ceef260c2f89b1caa7899846
marcelveldt <m.vanderveldt@outlook.com>
2016-03-08 23:38:49 +0100

    use sorttitle instead of just title

--- resources/lib/PluginContent.py
+++ resources/lib/PluginContent.py
@@ -605,7 +605,7 @@ def BROWSEGENRES(limit, type="movie"):
     count = 0
     allItems = []
     
-    sort = '"order": "ascending", "method": "title"'
+    sort = '"order": "ascending", "method": "sorttitle"'
     if "random" in type:
         sort = '"order": "descending", "method": "random"'
         type = type.replace("random","")
--- resources/lib/WebService.py
+++ resources/lib/WebService.py
@@ -179,7 +179,7 @@ class StoppableHttpRequestHandler (SimpleHTTPServer.SimpleHTTPRequestHandler):
             if cache: 
                 artwork = eval(cache)
             else:
-                sort = '"order": "ascending", "method": "title"'
+                sort = '"order": "ascending", "method": "sorttitle"'
                 if "random" in action:
                     sort = '"order": "descending", "method": "random"'
                     action = action.replace("random","")
